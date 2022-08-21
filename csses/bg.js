8a5713272ab869b86af05aacc5d0d43ed9e8bfd0
Ninecloud2077 <ncjack2077@163.com>
2022-08-21 21:05:23 +0800

    尝试把group类移近js

--- csses/bg.js
+++ csses/bg.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     $("div.jumbotron p").addclass("display-5");
     $("small").addclass("text-muted");
+    $("div.list-group a").addClass("list-group-item");
+    $("div.list-group a").addClass("list-group-item-action");
+    $("div.list-group a").addClass("list-group-item-secondary");
 });
