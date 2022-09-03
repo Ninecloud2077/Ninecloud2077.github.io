52fe5c7e93738b74903faabff7fc6fb4111422fc
Ninecloud2077 <ncjack2077@163.com>
2022-09-03 18:08:30 +0800

    Frame改形式

--- csses/bg.js
+++ csses/bg.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-    $("div.jumbotron p").addclass("display-5");
-    $("small").addclass("text-muted");
-    $("div.list-group a").addClass("list-group-item");
-    $("div.list-group a").addClass("list-group-item-action");
-    $("div.list-group a").addClass("list-group-item-secondary");
+    $("a.nav-link").addClass("display-5");
 });
