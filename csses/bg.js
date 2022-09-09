5ad3dc8bed9774d2ed28dea3c51a5cea7f770670
Ninecloud2077 <ncjack2077@163.com>
2022-09-09 15:02:04 +0800

    从各html的数组中获取Navbar的链接文本

--- csses/bg.js
+++ csses/bg.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
-    $.ajax({url:"https://ninecloud2077.github.io/scripts/Navbar.html",success:function(result){
+    $.ajax({url:"https://ninecloud2077.github.io/scripts/Navbar.html",
+    success:function(result){
         $("#head").after(result);
-    }});
-    $.ajax({url:"https://ninecloud2077.github.io/scripts/End.html",success:function(result){
+    }
+    });
+    $.ajax({url:"https://ninecloud2077.github.io/scripts/End.html",
+    success:function(result){
         $("#body").after(result);
-    }});
+    }
+    });
+});
 
+function LinkName(){
+    for(var i=0;i<$("a.nav-link").length;i++)
+    {
+      $("a.nav-link").eq(i).text(links[i])
+    }
+};
+
+function SetClasses(){
     $("#browser").addClass("navbar navbar-dark bg-dark");
     $("#head").addClass("jumbotron bg-secondary text-center text-white");
     $("#end").addClass("jumbotron text-center text-secondary");
     $("#body").addClass("container");
+};
+
+$(document).ajaxStop(function(){
+    SetClasses();
+    LinkName();
 });
