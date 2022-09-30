196c064bebbfc9d20c01f894d36f8aaeff9f4f8d
Ninecloud2077 <ncjack2077@163.com>
2022-09-30 18:05:33 +0800

    基础的location search搜索曲目

--- games/music/search.js
+++ games/music/search.js
@@ -8,7 +8,31 @@ function SearchSplit(Locate){
     }
     return LocaObj;
 }
+
 $(document).ready(function(){
     var Song=SearchSplit(location.search).s;
-    alert(Song);
+    if (!Song)
+    {
+        $("#body").text("查询出现错误！")
+    }else{
+        var url="https://ninecloud2077.github.io/games/music/songs/"+Song+".json";
+        var infos=["name","others","author"];
+        var passages=["songabout","gameabout"];
+        $.ajax({url:url,
+        success:function(result){
+            for(var i=0;i<infos.length;i++){
+                $("#"+infos[i]).text(function(n,o){
+                    return o+result["info"][infos[i]];
+                })
+            };
+            for(var i=0;i<passages.length;i++){
+                $("#"+passages[i]).load(result.passages,"#"+passages[i]+"-temp")
+            };
+            $("#iframe").attr("src",result.iframe);
+            $("#image").attr("src","songs/"+result.image);
+
+        }
+        });
+    }
+
 });
